3f8bab00507ffbd168a78b0483a63aacaf25cb21
rvedadi <ramtin.vedadi@gmail.com>
2020-06-10 11:58:02 +0430

    cors origin function

--- src/main/java/com/pody/PodyApplication.java
+++ src/main/java/com/pody/PodyApplication.java
@@ -11,6 +11,8 @@ import org.springframework.context.ApplicationContextAware;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableJpaRepositories(basePackages = {"com.pody.repository", "com.pody.repository.alt"})
 @EntityScan(basePackages = {"com.pody.model"})
@@ -34,19 +36,19 @@ public class PodyApplication extends SpringBootServletInitializer implements App
         modelMapper.getConfiguration().setSkipNullEnabled(true);
         return modelMapper;
     }
-//
-//    @Bean
-//    public WebMvcConfigurer corsConfigurer() {
-//        return new WebMvcConfigurer() {
-//            @Override
-//            public void addCorsMappings(CorsRegistry registry) {
-//                registry.addMapping("/**")
-//                        .allowedOrigins("http://localhost:8080")
-//                        .allowedMethods("GET", "POST", "DELETE", "PUT")
-//                        .allowCredentials(false);
-//            }
-//        };
-//    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedOrigins("http://pody.ir")
+                        .allowedMethods("GET", "POST", "DELETE", "PUT")
+                        .allowCredentials(false);
+            }
+        };
+    }
 }
 
 //package com.pody;
